bb92af4287f1275c1b75c90c49403815474b16d9
aceptaufikn <aceptaufik08@gmail.com>
2024-06-21 08:05:54 +0700

    add tugas 9

---
+++ Tugas9/src/utils/env.ts
@@ -0,0 +1,6 @@
+export const CLOUDINARY_API_KEY: string = process.env.CLOUDINARY_API_KEY || "";
+export const CLOUDINARY_API_SECRET: string =
+  process.env.CLOUDINARY_API_SECRET || "";
+export const CLOUDINARY_CLOUD_NAME: string =
+  process.env.CLOUDINARY_CLOUD_NAME || "";
+export const DATABASE_URL: string = "mongodb://takaserve:Takasrv@ac-iied6yg-shard-00-00.x81l1al.mongodb.net:27017,ac-iied6yg-shard-00-01.x81l1al.mongodb.net:27017,ac-iied6yg-shard-00-02.x81l1al.mongodb.net:27017/?ssl=true&replicaSet=atlas-vyzi3k-shard-0&authSource=admin&retryWrites=true&w=majority&appName=Cluster0";
